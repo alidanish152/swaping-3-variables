4651e7bf73eadcc940c69f7eed462e4cfee2194d
alidanish152 <muhammeddanishali@gmail.com>
2024-12-05 15:23:31 +0500

    l3

---
+++ main.py
@@ -0,0 +1,12 @@
+a=34
+b=43
+c=89
+print("orignal values are given below:")
+print("a=",a)
+print("b=",b)
+print("c=",c)
+a,b,c=c,b,a
+print("now there are new values:")
+print("a=",a)
+print("b=",b)
+print("c=",c)
